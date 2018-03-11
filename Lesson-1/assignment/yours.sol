7276bb980ab726f299c156e008fef02bcd22d7a3
Ivan Xia <xiacheyunivan@gmail.com>
2018-03-11 16:32:35 -0700

    [assignment-1] upload initial answer.
    Complete the Payroll contract and added support for updating employee and salary.

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,75 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint public constant payDuration = 1 minutes;
+
+    address public owner;
+
+    uint public salary;
+    address public employee;
+    uint public lastPayday;
+    
+    modifier ownerOnly() {require (msg.sender == owner); _;}
+    modifier employeeOnly() {require (msg.sender == employee); _;}
+
+    function Payroll(address e, uint s) public {
+        owner = msg.sender;
+        updateEmployee_(e, s);
+    }
+    
+    function updateEmployee(address e, uint s) public ownerOnly {
+        updateEmployee_(e, s);
+    }
+    
+    function updateEmployee_(address e, uint s) private {
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+        
+        require (e != 0x0 && s >= 0);
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+    
+    function addFund() public payable returns (uint) {
+        address contractAddress = this;
+        return contractAddress.balance;
+    }
+    
+    function calculateRunway() public constant returns (uint) {
+        address contractAddress = this;
+        return contractAddress.balance / salary;
+    }
+    
+    function hasEnoughFund() public constant returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function timesToBePaid() public constant returns (uint) {
+        return (now - lastPayday) / payDuration;
+    }
+    
+    function totalToBePaid() public constant returns (uint) {
+        return salary * timesToBePaid();
+    }
+    
+    function getPaidForOnce() public employeeOnly returns (uint) {
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+        return employee.balance;
+    }
+    
+    function getRemainingBalance() public constant returns (uint) {
+        address contractAddress = this;
+        return contractAddress.balance;
+    }
+    
+    function getEmployeeBalance() public constant returns (uint) {
+        return employee.balance;
+    }
+}
