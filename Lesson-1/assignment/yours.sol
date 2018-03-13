e4b99b5aca31c9bde417d68b0f5c5d6fa19872a2
Antipas <905002073@qq.com>
2018-03-13 14:27:59 +0800

    format

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,49 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+contract Payroll {
+    uint salary = 0;
+    address staff = 0x0;
+    uint constant payDuration = 30 days;
+    uint lastPayday = now;
+    
+    function addFun() payable returns (uint){
+        return this.balance;
+    }
+    
+    function hasEnoughFund() returns (bool){
+        return this.balance  / salary > 0;
+    }
+    
+    function setStaffAddress(address staffAddress){
+        require(salary != 0);
+        
+        staff = staffAddress;
+
+        //  清算
+        uint money = salary * ((now - lastPayday) / payDuration);
+        
+        require(money < this.balance);
+        lastPayday = now;
+        
+        if(money > 0){
+            staff.transfer(money);
+        }
+    }
+    
+    function setStaffPayroll(uint staffPayroll){
+        uint ss = staffPayroll * 1 ether;
+        require(ss != 0);
+        
+        salary = ss;
+    }
+    
+    function getPaid() {
+        require(staff != 0x0);
+        require(hasEnoughFund());
+        
+        uint nextPayDay = lastPayday - payDuration;
+        require(nextPayDay < now);
+        
+        lastPayday = nextPayDay;
+        staff.transfer(salary);
+    }
+}
