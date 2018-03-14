ca262e6755e461f5374ce30071479aafff1bddf3
Cheyun (Ivan) Xia <xiacheyunivan@gmail.com>
2018-03-13 22:30:29 -0700

    [assignment-1] Split two functions to update employee and salary respectively.

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,10 +1,11 @@
 pragma solidity ^0.4.14;
 
 contract Payroll {
-    uint public constant payDuration = 1 minutes;
-
+    event UpdateEmployee(address addressToPay, uint balance);
+    event UpdateSalary(address addressToPay, uint balance);
+    
+    uint public constant payDuration = 10 seconds;
     address public owner;
-
     uint public salary;
     address public employee;
     uint public lastPayday;
@@ -13,24 +14,37 @@ contract Payroll {
     modifier employeeOnly() {require (msg.sender == employee); _;}
 
     function Payroll(address e, uint s) public {
+        require(e != 0x0 && s > 0);
         owner = msg.sender;
-        updateEmployee_(e, s);
-    }
-    
-    function updateEmployee(address e, uint s) public ownerOnly {
-        updateEmployee_(e, s);
-    }
-    
-    function updateEmployee_(address e, uint s) private {
-        if (employee != 0x0) {
-            uint payment = salary * (now - lastPayday) / payDuration;
-            employee.transfer(payment);
-        }
-        
-        require (e != 0x0 && s >= 0);
         employee = e;
         salary = s * 1 ether;
         lastPayday = now;
+    }
+    
+    function updateEmployee(address e) public ownerOnly {
+        require(e != 0x0);
+        uint outStandingBalance = salary * (now - lastPayday) / payDuration;
+        address addressToPay = employee;
+        employee = e;
+        lastPayday = now;
+        safelyPayTheOutstandingBalance_(addressToPay, outStandingBalance);
+        emit UpdateEmployee(addressToPay, outStandingBalance);
+    }
+    
+    function updateSalary(uint s) public ownerOnly {
+        require (s >= 0);
+        uint outStandingBalance = salary * (now - lastPayday) / payDuration;
+        address addressToPay = employee;
+        salary = s * 1 ether;
+        lastPayday = now;
+        safelyPayTheOutstandingBalance_(addressToPay, outStandingBalance);
+        emit UpdateSalary(addressToPay, outStandingBalance);
+    }
+    
+    function safelyPayTheOutstandingBalance_(address add, uint amount) private {
+        if (add != 0x0 && amount > 0) {
+            add.transfer(amount);
+        }
     }
     
     function addFund() public payable returns (uint) {
@@ -47,22 +61,24 @@ contract Payroll {
         return calculateRunway() > 0;
     }
     
-    /* The employee calls this method to get his payment *FOR ONCE*. This method
-    * can be called multiple times if timesToBePaid() > 0.
-    *
-    */
+    /** The employee calls this method to get his payment *FOR ONCE*. This
+     * method can be called multiple times if timesToBePaid() > 0.
+     */
     function getPaid() public employeeOnly returns (uint) {
-        uint nextPayday = lastPayday + payDuration;
-        assert(nextPayday < now);
-
-        lastPayday = nextPayday;
-        employee.transfer(salary);
-        return employee.balance;
+        require (nextPayday < now);
+        if (employee != 0x0 && salary > 0) {
+            uint nextPayday = lastPayday + payDuration;
+            lastPayday = nextPayday;
+            employee.transfer(salary);
+            return employee.balance;
+        }
+        return 0;
     }
     
     // =========================================================================
-    // Convenient public methods for debugging purpose.
+    // Helper functions for debugging purpose
     // =========================================================================
+    /*
     function timesToBePaid() public constant returns (uint) {
         return (now - lastPayday) / payDuration;
     }
@@ -70,13 +86,5 @@ contract Payroll {
     function totalToBePaid() public constant returns (uint) {
         return salary * timesToBePaid();
     }
-    
-    function getRemainingBalance() public constant returns (uint) {
-        address contractAddress = this;
-        return contractAddress.balance;
-    }
-    
-    function getEmployeeBalance() public constant returns (uint) {
-        return employee.balance;
-    }
+    */
 }
