1b4a058a3dcf376dfca01862960fde9159b48530
liaohuqiu <liaohuqiu@gmail.com>
2018-03-21 16:33:57 +0800

    remove useless comment

--- Lesson-3/assignment/Payroll.sol
+++ Lesson-3/assignment/Payroll.sol
@@ -19,17 +19,17 @@ contract Payroll is Ownable {
         require(msg.sender == owner);
         _;
     }
-    
+
     modifier shouldExist(address employeeId) {
         assert(employees[employeeId].lastPayday != 0);
         _;
     }
-    
+
     modifier shouldNotExist(address employeeId) {
         assert(employees[employeeId].lastPayday == 0);
         _;
     }
-    
+
     uint constant payDuration = 10 seconds;
     uint public totalSalary = 0;
 
@@ -42,43 +42,41 @@ contract Payroll is Ownable {
     function Payroll() payable public Ownable {
         owner = msg.sender;
     }
-    
+
     function _partialPaid(address employeeId) private {
-        uint payment = employees[employeeId].salary * 
-            (now - employees[employeeId].lastPayday) / payDuration;
+        uint payment = employees[employeeId].salary
+            .mul(now.sub(employees[employeeId].lastPayday))
+            .div(payDuration);
         employeeId.transfer(payment);
     }
-    
-    function addEmployee(address employeeId, uint salary) public 
+
+    function addEmployee(address employeeId, uint salary) public
             onlyOwner shouldNotExist(employeeId) {
         salary = salary.mul(1 ether);
         employees[employeeId] = Employee(salary, now);
-        
+
         totalSalary = totalSalary.add(salary);
     }
-    
-    function removeEmployee(address employeeId) public 
+
+    function removeEmployee(address employeeId) public
             onlyOwner shouldExist(employeeId) {
-        // pay first        
         _partialPaid(employeeId);
 
-        // update totalSalary
         uint salary = employees[employeeId].salary;
-        totalSalary= totalSalary.sub(salary);
-        
-        // remove from map
+        totalSalary = totalSalary.sub(salary);
+
         delete employees[employeeId];
     }
-    
+
     function changePaymentAddress(address oldAddress, address newAddress) public
             onlyOwner shouldExist(oldAddress) shouldNotExist(newAddress) {
         _partialPaid(oldAddress);
-        
+
         employees[newAddress] = Employee(employees[oldAddress].salary, now);
         delete employees[oldAddress];
     }
-    
-    function updateEmployee(address employeeId, uint salary) public 
+
+    function updateEmployee(address employeeId, uint salary) public
             onlyOwner shouldExist(employeeId) {
         _partialPaid(employeeId);
 
@@ -89,15 +87,15 @@ contract Payroll is Ownable {
         employees[employeeId].lastPayday = now;
         totalSalary = totalSalary.add(salary).sub(oldSalary);
     }
-    
+
     function addFund() payable public returns (uint) {
         return address(this).balance;
     }
-    
+
     function calculateRunway() public view returns (uint) {
         return address(this).balance.div(totalSalary);
     }
-    
+
     function hasEnoughFund() public view returns (bool) {
         return calculateRunway() > 0;
     }
@@ -105,7 +103,7 @@ contract Payroll is Ownable {
     function getPaid() public shouldExist(msg.sender) {
         address employeeId = msg.sender;
 
-        uint nextPayday = employees[employeeId].lastPayday + payDuration;
+        uint nextPayday = employees[employeeId].lastPayday.add(payDuration);
         assert(nextPayday < now);
 
         employees[employeeId].lastPayday = nextPayday;
