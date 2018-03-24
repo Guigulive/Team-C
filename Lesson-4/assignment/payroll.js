2bfcd7a37d9ad61dc486d32f1effae3b4ae4bb7a
Antipas <905002073@qq.com>
2018-03-24 13:42:36 +0800

    format

--- Lesson-4/assignment/payroll.js
+++ Lesson-4/assignment/payroll.js
@@ -19,16 +19,12 @@ contract('Payroll', function(accounts){
 	it('add Employee test....', function(){
 		return Payroll.deployed().then(function(instance){
 			payrollInstance = instance;
-
 			return payrollInstance.addEmployee(accounts[0], 1, {from: accounts[0]});
 		})
 		.then(() =>{
-
             return payrollInstance.employees.call(accounts[0]);
 		})
 		.then((employee) => {
-			// console.log(JSON.stringify(employee));
-
 			assert.equal(employee[1].valueOf(), web3.toWei(1), 'balance should be 1');
 		})
 	});
@@ -38,26 +34,20 @@ contract('Payroll', function(accounts){
 	it('getPaid test....', function(){
 		return Payroll.deployed().then(function(instance){
 			payrollInstance = instance;
-
 			return payrollInstance.addFund({from: accounts[0], value: web3.toWei(10, 'ether')});
 		})
 		.then(() =>{
 			return timeTravel(86400 * 31);
-
 		})
 		.then(() =>{
 			balanceInit = web3.eth.getBalance(accounts[0]).toNumber();
-
 			return payrollInstance.getPaid({from: accounts[0]}); 
 		})
 		.then(() =>{
-
 			return payrollInstance.employees.call(accounts[0]);
 		})
 		.then((employee) =>{
-
 			var gas = web3.eth.gasPrice * web3.eth.getBlock(web3.eth.blockNumber).gasUsed;
-
 			assert.isAbove((employee[1].toNumber() - gas), (web3.eth.getBalance(accounts[0]).toNumber() - balanceInit ), 'my salary is wrong');
 		})
 	});
@@ -65,19 +55,14 @@ contract('Payroll', function(accounts){
 	it('remove Employee test....', function(){
 		return Payroll.deployed().then(function(instance){
 			payrollInstance = instance;
-
 			return payrollInstance.removeEmployee(accounts[0], {from: accounts[0]});
 		})
 		.then(() =>{
-
             return payrollInstance.employees.call(accounts[0]);
 		})
 		.then((employee) => {
-			// console.log(JSON.stringify(employee));
-
 			assert.equal(employee[0].valueOf(), 0x0, 'address should be 0x0 after remove');
 		})
 	});
-
 
 });
