4a8cfb63bdbf14e9cfd2036b60dc48c76e7e4f06
liaohuqiu <liaohuqiu@gmail.com>
2018-03-28 20:50:02 +0800

    finish lesson-5

---
+++ Lesson-5/assignment/src/lesson-5/src/components/Employee.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react'
+import {Card, Col, Row, Layout, Alert, message, Button} from 'antd';
+
+import Common from './Common';
+
+class Employer extends Component {
+    checkEmployee = () => {
+        const {payroll, account, web3} = this.props;
+        payroll.getEmployeeInfoById.call(account, {
+            from: account
+        }).then((ret) => {
+            const info = {
+                salary: web3.fromWei(ret[0].toNumber()),
+                lastPaidDate: new Date(ret[1].toNumber() * 1000).toString(),
+                balance: web3.fromWei(ret[2].toNumber()),
+            }
+            console.log("checkEmployee");
+            console.log(info);
+            this.setState(info);
+        }).catch((error) => {
+            console.log(error);
+            message.error(error.message);
+        });
+    }
+
+    getPaid = () => {
+        const {payroll, account} = this.props;
+        payroll.getPaid.call({
+            from: account,
+        }).then((ret) => {
+            console.log("getPaid");
+            console.log(ret);
+        }).catch((error) => {
+            console.log(error);
+            message.error(error.message);
+        });
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            salary: 0,
+            lastPaidDate: '',
+            balance: false,
+        };
+    }
+
+    componentDidMount() {
+        this.checkEmployee();
+    }
+
+    renderContent() {
+        const {salary, lastPaidDate, balance} = this.state;
+
+        if (!salary || salary === '0') {
+            return <Alert message="你不是员工" type="error" showIcon/>;
+        }
+
+        return (
+            <div>
+                <Row gutter={16}>
+                    <Col span={8}>
+                        <Card title="薪水">{salary} Ether</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="上次支付">{lastPaidDate}</Card>
+                    </Col>
+                    <Col span={8}>
+                        <Card title="帐号金额">{balance} Ether</Card>
+                    </Col>
+                </Row>
+
+                <Button
+                    type="primary"
+                    icon="bank"
+                    onClick={this.getPaid}
+                >
+                    获得酬劳
+                </Button>
+            </div>
+        );
+    }
+
+    render() {
+        const {account, payroll, web3} = this.props;
+
+        return (
+            <Layout style={{padding: '0 24px', background: '#fff'}}>
+                <Common account={account} payroll={payroll} web3={web3}/>
+                <h2>个人信息</h2>
+                {this.renderContent()}
+            </Layout>
+        );
+    }
+}
+
+export default Employer
